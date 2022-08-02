b0ad2bf79401f354330ce985199b3935fa8f8ce4
BUI <623607878@qq.com>
2022-08-02 16:24:41 +0800

    测试

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = () => {
   let userCanvas = document.getElementById('canvas')
   let gridRender = new GridRenderEngine(userCanvas)
   function draw() {
-    gridRender.drawGrid()
+    gridRender.draw()
     gridRender.camera.posX += 0.5
     gridRender.camera.posY += 0.5
     window.requestAnimationFrame(draw)
